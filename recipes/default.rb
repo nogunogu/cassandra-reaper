910449ea6a6e1803ec405ef03f2018bc2ca0af48
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-10-01 16:08:58 +0900

    cassandra reaper default

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+cookbook_file '/tmp/reaper_1.2.2_amd64.deb' do
+  source 'reaper_1.2.2_amd64.deb'
+  owner 'root'
+  group 'root'
+  mode '0644'
+end
+
+dpkg_package 'reaper_1.2.2_amd64.deb' do
+  source '/tmp/reaper_1.2.2_amd64.deb'
+  action :install
+end
