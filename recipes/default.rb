09670ec35fff1d06e7abe228218e0579cffd168f
Koichi NOGUCHI <koichi.noguchi@rakuten.com>
2018-10-02 10:18:09 +0900

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,12 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+include_recipe 'java'
+
+# postgresql_server_install 'PostgreSQL server' do
+#   action :install
+# end
+
 cookbook_file '/tmp/reaper_1.2.2_amd64.deb' do
   source 'reaper_1.2.2_amd64.deb'
   owner 'root'
@@ -13,4 +19,8 @@ end
 dpkg_package 'reaper_1.2.2_amd64.deb' do
   source '/tmp/reaper_1.2.2_amd64.deb'
   action :install
+end
+
+service 'cassandra-reaper' do
+  action :start
 end
